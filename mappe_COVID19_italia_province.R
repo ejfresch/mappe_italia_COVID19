5e2111abac595d53bd6d769fd74d41677eca9c83
Luca Freschi <l.freschi@gmail.com>
2020-03-12 14:04:47 -0400

    Fixed the problem with the data file encoding (province). Will work until Mar 20

--- mappe_COVID19_italia_province.R
+++ mappe_COVID19_italia_province.R
@@ -21,7 +21,7 @@ date_zipped=format(current_date, format="%Y%m%d")
 msg=paste("Data: ",format(current_date, format="%Y-%m-%d"), "; Dati: Protezione Civile",sep="")
 
 # I read csv file
-if(current_f %in% c("dpc-covid19-ita-province-20200311.csv")){ 
+if(current_f %in% list.files("dati/dati_protezione_civile/COVID-19-master/dati-province/",pattern="-2020031[1-9]")){ 
 csv = read.csv(paste("dati/dati_protezione_civile/COVID-19-master/dati-province/",current_f,sep=""),check.names=FALSE, stringsAsFactors = FALSE , fileEncoding="ISO-8859-1")
 }else{
 csv = read.csv(paste("dati/dati_protezione_civile/COVID-19-master/dati-province/",current_f,sep=""),check.names=FALSE, stringsAsFactors = FALSE)
